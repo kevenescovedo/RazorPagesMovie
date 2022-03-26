ec475e630a593b6b490f316516bbd00020d5abab
DESKTOP-Q1VIMM1\keven <kevenescovedo1916@gmail.com>
2022-03-26 12:21:50 -0300

    Passo 1 ao 7

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [Display(Name = "Título")]
+        public string Title { get; set; } = string.Empty;
+
+        [Display(Name = "Data de Lançamento")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Display(Name = "Genêro")]
+        public string Genre { get; set; } = string.Empty;
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        [Display(Name = "Classificação")]
+        public string Rating { get; set; } = string.Empty;
+    }
+}
