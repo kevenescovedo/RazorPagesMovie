8cf30857f0ff8cbca4063869bf8ed2417d1e8663
DESKTOP-Q1VIMM1\keven <kevenescovedo1916@gmail.com>
2022-03-28 16:13:07 -0300

    Passo 8 finaizado e programa também

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -7,6 +7,9 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         [Display(Name = "Título")]
         public string Title { get; set; } = string.Empty;
 
@@ -15,9 +18,14 @@ namespace RazorPagesMovie.Models
         public DateTime ReleaseDate { get; set; }
         [Display(Name = "Genêro")]
         public string Genre { get; set; } = string.Empty;
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Display(Name = "Preço")]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        //[StringLength(5)]
+        [Required]
         [Display(Name = "Classificação")]
         public string Rating { get; set; } = string.Empty;
     }
--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -36,3 +37,4 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
